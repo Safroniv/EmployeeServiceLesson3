944b7763f800949c1da1d3668f3f919f286a16cf
Иван Сафронов <safroniv@mail.ru>
2022-10-12 08:45:48 +0300

    Add project files.

---
+++ EmployeeService/Services/Impl/DepartmentRepository.cs
@@ -0,0 +1,33 @@
+﻿using EmployeeService.Data;
+using EmployeeService.Models;
+
+namespace EmployeeService.Services.Impl
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        public Guid Create(Department data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Department> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Department GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Department data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
